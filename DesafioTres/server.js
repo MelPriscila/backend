0cd595a9513071f4af1a8bb5bf348b1b0abb489d
Melanie Lema <melanielema1805@gmail.com>
2022-11-23 23:38:24 -0300

    first commit

---
+++ DesafioTres/server.js
@@ -0,0 +1,28 @@
+import  express  from "express";
+import Contenedor from './contenedor.js';
+import fs from "fs";
+
+const app = express()
+const PORT = 8080;
+
+const server = app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Servidor HTTP escuchando en el puerto ${PORT}`);
+});
+
+server.on('error', err => console.log(`error: ${err}`));
+
+const products = new Contenedor("products.txt");
+
+app.get('/productos', async(req, res)=>{
+    const productos = await products.getAll();
+    res.send(productos);
+});
+
+app.get('/productoRandom', async(req, res)=>{
+    const productos = await products.getAll();
+    let numeroRandom = Math.floor(Math.random() * productos.length);
+    res.send(productos[numeroRandom]);
+});
+
+
+
